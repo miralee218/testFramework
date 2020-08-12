8bbb0c94c0b966170e8f57b0d39ac30d39ec84f4
MiraLee <m10509305@gapps.ntust.edu.tw>
2020-08-12 12:00:05 +0800

    Initial Commit

---
+++ testFramework/testFramework.h
@@ -0,0 +1,18 @@
+//
+//  testFramework.h
+//  testFramework
+//
+//  Created by 李雅雯 on 2020/8/12.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for testFramework.
+FOUNDATION_EXPORT double testFrameworkVersionNumber;
+
+//! Project version string for testFramework.
+FOUNDATION_EXPORT const unsigned char testFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <testFramework/PublicHeader.h>
+
+
